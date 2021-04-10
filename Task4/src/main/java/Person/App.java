9abcc9cdb2a23af705f7064598ae595a99973344
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-04-10 20:13:58 +0300

    correct Person

--- Task4/src/main/java/Person/App.java
+++ Task4/src/main/java/Person/App.java
@@ -5,9 +5,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -15,69 +15,50 @@ import java.util.List;
 public class App {
     public static void main(String[] args) throws IOException {
 
+        // group 100 persons with age
         List<Person> persons = MethodsForPerson.personList(15, 30);
-        List<Person> personsWithAge = MethodsForPerson.personListWithAge(21, persons);
-        //
+
+        // persons with less age
+        List<Person> personsWithAge = persons.stream().filter(x -> (x.getAge() <= 21)).collect(Collectors.toList());
+
+        // print persons
         for (Person person : personsWithAge) {
             System.out.println(person.toString());
         }
+        System.out.println("----------------------------------");
 
-        // sort
-        Comparator<Person> byName = new PersonNameComparator();
-        Comparator<Person> bySurName = new PersonSurNameComparator();
-        Collections.sort(personsWithAge, bySurName);
-        Collections.sort(personsWithAge, byName);
+        // sort by surname and name
+        List<Person> sortPersons = personsWithAge.stream()
+                .sorted(Comparator.comparing(Person::getSurName).thenComparing(Person::getName))
+                .collect(Collectors.toList());
 
-//delete dublicate
-        List<Person> withoutDublicateListPersons = MethodsForPerson.withoutDuble(personsWithAge);
-        System.out.println(withoutDublicateListPersons.size());
+        // delete dublicate
+        List<Person> notDoublePerson = sortPersons.stream().distinct().collect(Collectors.toList());
 
-        FileWriter writer = new FileWriter("output.txt");
-        for (Person person : withoutDublicateListPersons) {
+        // write file
+        FileWriter writer = new FileWriter("Task4\\fileText.txt");
+        for (Person person : notDoublePerson) {
             String name = person.getName();
             String surName = person.getSurName();
             String age = String.valueOf(person.getAge());
-            writer.write(name +
-                    " " + surName +
-                    " " + age + " ");
+            writer.write("surName = " + surName +
+                    ", name = " + name +
+                    ", age = " + age + "\n");
         }
         writer.close();
 
-        BufferedReader br = new BufferedReader(new FileReader("output.txt"));
-        String[] line = br.readLine().split(" ");
-        List<String> names = new ArrayList<>();
-        List<String> surNames = new ArrayList<>();
-        for (int i = 0; i < line.length; i++) {
-            if ((i + 1) % 3 == 0) {
-            } else if ((i + 1) % 2 == 0) {
-                surNames.add(line[i]);
-            } else if ((i + 1) % 1 == 0) {
-                names.add(line[i]);
-            }
+        // read file
+        BufferedReader br = new BufferedReader(new FileReader("Task4\\fileText.txt"));
+        String text;
+        List<String> listNames = new ArrayList<>();
+        while ((text = br.readLine()) != null) {
+            String[] line = text.split(",");
+            String names = line[0] + "," + line[1];
+            listNames.add(names);
         }
         br.close();
 
-        String[] arrName = new String[names.size()];
-        String[] arrSurName = new String[surNames.size()];
-
-        for (int i = 0; i < names.size(); i++) {
-            arrName[i] = names.get(i);
-        }
-        for (int i = 0; i < surNames.size(); i++) {
-            arrSurName[i] = surNames.get(i);
-        }
-
-
-        List<String> nameAndSurName = new ArrayList<>();
-        for (int i = 0; i < arrName.length; i++) {
-            nameAndSurName.add(arrName[i] + " " + arrSurName[i]);
-        }
-
-        for (String s : nameAndSurName) {
-            System.out.println(s);
-        }
-        System.out.println(names.size());
-        System.out.println(surNames.size());
-
+        // print surname and name
+        listNames.forEach(System.out::println);
     }
 }
--- Task4/src/main/java/Person/MethodsForPerson.java
+++ Task4/src/main/java/Person/MethodsForPerson.java
@@ -2,43 +2,31 @@ package Person;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MethodsForPerson {
 
-    private static String getRandomNameOrSurName() {
-        return String.copyValueOf(new char[]{(char) ('a' + Math.random() * ('z' - 'a' + 1))});
+    private static String getRandomName() {
+        String[] names = {"Ivan", "Fedor", "Alex", "Boris", "Semen", "Vasya", "Kolya"};
+        int randomName = (int) (Math.random() * 7);
+        return names[randomName];
+    }
+
+    private static String getRandomSurName() {
+        String[] names = {"Ivanov", "Fedorov", "Alexandrov", "Borisov", "Semenov", "Vasiliev", "Nikolaev"};
+        int randomSurName = (int) (Math.random() * 7);
+        return names[randomSurName];
     }
 
     static List<Person> personList(int minAge, int maxAge) {
         List<Person> personList = new ArrayList<>();
-        for (int i = 0; i <= 100; i++) {
-            int age = (int) (Math.random() * (maxAge + 1)) + minAge;
-            Person person = new Person(getRandomNameOrSurName(), getRandomNameOrSurName(), age);
+        Random random = new Random();
+        int diff = maxAge - minAge;
+        for (int i = 0; i < 100; i++) {
+            int age = random.nextInt(diff + 1) + minAge;
+            Person person = new Person(getRandomName(), getRandomSurName(), age);
             personList.add(person);
         }
         return personList;
-    }
-
-    static List<Person> personListWithAge(int age, List<Person> personsList) {
-        List<Person> persons = new ArrayList<>();
-        for (Person person : personsList) {
-            if (person.getAge() < age) {
-                persons.add(person);
-            }
-        }
-        return persons;
-    }
-
-    static List<Person> withoutDuble(List<Person> personsList) {
-        for (int i = 0; i < personsList.size(); i++) {
-            for (int j = 0; j < personsList.size(); j++) {
-                if (i == j) {
-
-                } else if (personsList.get(j).equals(personsList.get(i))) {
-                    personsList.remove(j);
-                }
-            }
-        }
-        return personsList;
     }
 }
