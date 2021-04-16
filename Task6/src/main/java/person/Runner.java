c1aadb053e864d9fab7eb66fd2535a29663b4713
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-04-16 23:10:55 +0300

    create maven project

---
+++ Task6/src/main/java/person/Runner.java
@@ -0,0 +1,11 @@
+package person;
+
+/**
+ * Hello world!
+ */
+public class Runner {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ Task6/src/test/java/person/RunnerTest.java
@@ -0,0 +1,18 @@
+package person;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple Runner.
+ */
+public class RunnerTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
