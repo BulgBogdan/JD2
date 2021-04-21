446e9f0a5fe2d6c99dee148b7a9896d814cf19f6
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-04-21 21:17:08 +0300

    added methods close

--- Task6/src/main/java/person/implement/PersonDBImpl.java
+++ Task6/src/main/java/person/implement/PersonDBImpl.java
@@ -10,14 +10,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PersonDBImpl implements PersonService {
 
     @Override
     public void createPerson(List<Person> persons) {
+        Connection connection = null;
+        Statement statement = null;
         try {
-            Connection connection = ConnectorCreator.getConnection();
-            Statement statement = connection.createStatement();
+            connection = ConnectorCreator.getConnection();
+            statement = connection.createStatement();
             for (Person person : persons) {
                 String sql = String.format("INSERT INTO `People`.`Person`" +
                                 "(name, sur_name, age, salary, passport, address, " +
@@ -35,21 +38,25 @@ public class PersonDBImpl implements PersonService {
                         person.getLetter());
                 statement.executeUpdate(sql);
             }
-            ConnectorCreator.closeConnection();
-            statement.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            blockFinallyClose(connection, statement);
         }
     }
 
     @Override
     public List<Person> getAllPerson() {
         List<Person> result = new ArrayList<>();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet rs = null;
         try {
-            Connection connection = ConnectorCreator.getConnection();
-            Statement statement = connection.createStatement();
+            connection = ConnectorCreator.getConnection();
+            statement = connection.createStatement();
             String sql = "SELECT * FROM `People`.`Person`";
-            ResultSet rs = statement.executeQuery(sql);
+            rs = statement.executeQuery(sql);
             while (rs.next()) {
                 Person person = new Person(
                         rs.getInt("id"),
@@ -70,8 +77,30 @@ public class PersonDBImpl implements PersonService {
             statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            blockFinallyClose(connection, statement);
+            try {
+                if (Objects.nonNull(rs)) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+
         return result;
     }
 
+    private void blockFinallyClose(Connection connection, Statement statement) {
+        try {
+            if (Objects.nonNull(connection)) {
+                ConnectorCreator.closeConnection();
+            }
+            if (Objects.nonNull(statement)) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
